0759f1ca1fd0353b8a8393a1a498cb6594a88390
Cythilya Tang <cythilya@gmail.com>
2017-07-17 22:01:25 +0800

    [modify] fix some bugs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-//var path = require('path');
+var path = require('path');
 var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
@@ -8,11 +8,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.urlencoded({limit: '5mb'}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
